81f9143e21de561e2ffe3a03ac86a9ba262fc2ce
Indy Peters <46998095+IndyDPeters@users.noreply.github.com>
2022-07-09 04:34:04 +0200

    Hello

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+var pageName = app.get("/:name", (req, res) => {});
+
+app.post('/:pageName',(req, res) => {
+ res.send('page ' + req.params.pageName + ' was not found')
+});
+
+app.post('/', (req, res) => {
+  res.json(req.body);
+});
+
+
+app.use(express.json());
 
 app.use(express.static(__dirname + '/static'));
 
@@ -9,8 +22,22 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static/index.html');
 });
 
+app.get('/secondPage', (req, res) => {
+  res.sendFile(__dirname + '/static/secondPage.html');
+});
+
+app.get("/:name", (req, res) => {
+  res.send("Your name is " + req.params.name + "\n");
+});
+
+app.post('/', (req, res) => {
+  res.json(req.body);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
   console.log(`http://localhost:${port}`)
 })
+
